906bec1bd3d84b40a8a6c185184edd19f8be3906
vivek <vivek.sirsath1@gmail.com>
2024-02-21 01:10:15 +0530

    ReverseEachWord_In_String_028 added

---
+++ SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/ReverseEachWord_In_String_028.java
@@ -0,0 +1,28 @@
+package frequentlyAskedPrograms;
+
+public class ReverseEachWord_In_String_028 {
+
+	public static void main(String[] args) {
+
+		String str = "Welcome To Java";
+
+		String[] words = str.split(" "); // Splitting string into words
+
+		String reverseString = "";
+
+		for (String w : words) {
+
+			String reverseWord = "";
+
+			for (int i = w.length() - 1; i >= 0; i--) {
+				reverseWord = reverseWord + w.charAt(i);
+			}
+
+			reverseString = reverseString + reverseWord + " ";
+		}
+
+		System.out.println(reverseString); // emocleW oT avaJ
+
+	}
+
+}
