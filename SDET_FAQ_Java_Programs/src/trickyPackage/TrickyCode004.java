01fb8b79d97858524b6c303960466610b58e6ebe
vivek <vivek.sirsath1@gmail.com>
2024-02-23 23:17:06 +0530

    Logical operators AND (&&), OR (||): Truth Table added

--- SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode004.java
+++ SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode004.java
@@ -1,7 +1,32 @@
 package trickyPackage;
 
 /* Q. What will happen when we compile and run the following code?
- * Answer - false
+   Answer - false
+  
+  Truth Tables:
+  ============
+  Logical AND ('&&') operator:
+  ===========================
+  This operator returns true only if both operands are true, otherwise it returns false.
+  
+  true	&& true	- true
+  true	&& false - false
+  false	&& true - false
+  false	&& false - false
+  
+  ======================================================================================
+  
+  Logical OR ('||') operator:
+  ===========================
+  This operator returns true if at least one of the operands is true, otherwise it returns false.
+  
+  true  || true - true
+  true  || false - true
+  false	|| true	- true
+  false || false - false
+
+   So, 'true && false && !true' simplifies to 'true && false && false', which ultimately evaluates to false.
+   
  */
 
 public class TrickyCode004 {
@@ -10,7 +35,7 @@ public class TrickyCode004 {
 
 		boolean b1 = true, b2 = false, b3 = true;
 
-		System.out.println(b1 && b2 && !b3);  // (true && false && true) = false
+		System.out.println(b1 && b2 && !b3); // (true && false && false) = false
 
 	}
 
