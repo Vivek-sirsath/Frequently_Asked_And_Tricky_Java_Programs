7e27154322993b5b005798e121e7fe44140c0fb3
vivek <vivek.sirsath1@gmail.com>
2024-03-05 22:30:50 +0530

    TrickyCode013 modified and 013_01 added

--- SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode013_01.java
+++ SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode013_01.java
@@ -3,7 +3,7 @@ package trickyPackage;
 //Q. What will happen in the following code
 //Answer: Syntax error in Line no 8, while is keyword cann't use as class name
 
-public class TrickyCode013 {
+public class TrickyCode013_01 {
 
 	public class while { // line 8 
 
