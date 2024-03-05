d56f07951e06a0e5e8a9faffd77754f433bb6b5d
vivek <vivek.sirsath1@gmail.com>
2024-03-05 22:28:43 +0530

    TrickyCode005 modified

--- SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode005.java
+++ SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode005.java
@@ -9,7 +9,7 @@ public class TrickyCode005 {
 	public static void main(String[] args) {
 		
 		int i = 0;
-		int j = i++ + ++i;
+		int j = i++ + ++i; // 0 1 + 2 2 = 0+2 = 2
 		
 		System.out.println(j);   // 2
 
