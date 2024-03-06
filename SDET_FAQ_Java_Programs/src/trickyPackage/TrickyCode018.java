54cbd279ffd93328171f975cef494e171757b758
vivek <vivek.sirsath1@gmail.com>
2024-03-06 23:35:03 +0530

    TrickyCode018 - Recursion Example Added

--- SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode018.java
+++ SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode018.java
@@ -1,7 +1,12 @@
 package trickyPackage;
 
-//Q. What is the output of the following code
-//Answer: Output is 1
+/*  Q. What is the output of the following code
+    Answer: Output is 1
+    
+    Recursion is a programming technique where a function calls itself, 
+    either directly or indirectly, to solve a problem.
+    Here, recursion occurs within the func() method.
+*/
 
 // Instead of TrickyCode018 class name should be Recursion
 class TrickyCode018 {
@@ -14,6 +19,7 @@ class TrickyCode018 {
 		result = func(n - 1);
 		return result;
 	}
+
 	public static void main(String[] args) {
 
 		TrickyCode018 obj = new TrickyCode018();
@@ -23,6 +29,10 @@ class TrickyCode018 {
 
 class Output {
 
-	// main method should be here according to Original question
-	
+	/*
+	 * main method should be here according to Original question 
+	 * Insted of, TrickyCode018 obj = new TrickyCode018();
+	 * Recursion obj = new Recursion(); 
+	 * System.out.println(obj.func(5));
+	 */
 }
