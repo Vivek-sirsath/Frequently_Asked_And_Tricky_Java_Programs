7ab624af5ed03b97931a2357f25c8d24c542a83e
vivek <vivek.sirsath1@gmail.com>
2025-02-21 00:38:52 +0530

    Array with blank output program added

---
+++ SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/ArrayBlankOutput.java
@@ -0,0 +1,12 @@
+package frequentlyAskedPrograms;
+
+public class ArrayBlankOutput {
+
+	public static void main(String[] args) {
+
+		char charArray[] = new char[2];
+		System.out.println(charArray[1]);
+	}
+
+}
+// Blank Output
