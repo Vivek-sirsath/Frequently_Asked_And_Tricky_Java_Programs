6ff54b57424bc195fdadab59d50443624be289f2
vivek <vivek.sirsath1@gmail.com>
2025-02-27 23:04:20 +0530

    TrickyCode035 added

---
+++ SDET_FAQ_Java_Programs/src/trickyPackage/TrickyCode035.java
@@ -0,0 +1,22 @@
+
+/*
+Q. What will be the output of the following code?
+   Answer: Compilation Error
+   
+   Explanation:
+   The variable declared is a and the one used in System.out.println() function is x 
+   which is not declared, hence we'll get compilation error.
+*/
+
+package trickyPackage;
+
+public class TrickyCode035 {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+//		System.out.println(!(x > 5 && x < 10) ? 1 : 0);
+
+	}
+
+}
