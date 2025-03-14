93627427daced22d5eed733073a7d15b7773b8de
vivek <vivek.sirsath1@gmail.com>
2025-03-14 16:04:17 +0530

    Modified file EqualityOfArray_016_02.java

--- SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/EqualityOfArray_016_02.java
+++ SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/EqualityOfArray_016_02.java
@@ -5,27 +5,27 @@ package frequentlyAskedPrograms;
 public class EqualityOfArray_016_02 {
 
 	public static void main(String[] args) {
-		
-		int[] a1 = {1,2,5,4,5,7};
-		int[] a2 = {1,2,3,4,5};
-		
+
+		int[] a = { 1, 2, 3, 4, 5, 6 };
+		int[] b = { 1, 2, 3, 4, 5 };
+
 		boolean status = true;
-		if(a1.length == a2.length) {
-			
-			for(int i = 0; i < a1.length; i++) {
-				
-				if(a1[i] != a2[i]) {
+		if (a.length == b.length) {
+
+			for (int i = 0; i < a.length; i++) {
+
+				if (a[i] != b[i]) {
 					status = false;
 				}
 			}
-			
-		}else {
+
+		} else {
 			status = false;
 		}
-		
-		if(status==true) {
+
+		if (status == true) {
 			System.out.println("Arrays are Equal");
-		}else {
+		} else {
 			System.out.println("Arrays are NOT Equal");
 		}
 
