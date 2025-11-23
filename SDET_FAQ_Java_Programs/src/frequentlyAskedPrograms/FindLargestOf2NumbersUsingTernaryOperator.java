3c0470c8eb7c0438f9f228abdd48b08fd1996ff2
vivek <vivek.sirsath1@gmail.com>
2025-11-24 01:12:38 +0530

    Modified FindLargestOf2NumbersUsingTernaryOperator.java for Both conditions

--- SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/FindLargestOf2NumbersUsingTernaryOperator.java
+++ SDET_FAQ_Java_Programs/src/frequentlyAskedPrograms/FindLargestOf2NumbersUsingTernaryOperator.java
@@ -3,6 +3,10 @@
   SYNTAX :
   ------
            result = (condition) ? value_if_true : value_if_false;
+           
+           i.e. 
+           
+           result = (condition) ? If_True Code_1 execute : If_False Code_2 execute
 
 */
 
@@ -12,10 +16,16 @@ public class FindLargestOf2NumbersUsingTernaryOperator {
 
 	public static void main(String[] args) {
 
-		int a = 20, b = 30;
-
+		int a = 50, b = 30;
+		
+		// 1st way
 		String result = (a > b) ? "a is largest number" : "b is largest number";
 		System.out.println(result);
+		
+		
+		// 2nd way
+		String res = (a < b) ? "b is largest number" : "a is largest number";
+		System.out.println(res);
 
 	}
 
